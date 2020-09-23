15e2bc04b10df6414c3aa41bd39679249a112674
raudor <rivaspaulino@outlook.com>
2020-09-23 10:51:01 -0700

    more multiple welcomes

---
+++ 0x0C-Javascript_advanced/2-function_me.js
@@ -0,0 +1,12 @@
+function welcomeMessage(fullName) {
+    function AlterMessage() {
+        alert('Welcome ' + fullName);
+    };
+    return AlterMessage;
+}
+let guillaume = welcomeMessage('Guillaume');
+let alex = welcomeMessage('Alex');
+let fred = welcomeMessage('Fred');
+guillaume();
+alex();
+fred();
